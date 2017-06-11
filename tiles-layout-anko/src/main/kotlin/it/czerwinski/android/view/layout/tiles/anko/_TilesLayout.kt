eb20501b3651c2ad219216f8e0e6cce1279266f3
Slawomir Czerwinski <slawomir@czerwinski.it>
2017-06-11 02:49:03 +0100

    Improved padding properties in Anko view

--- tiles-layout-anko/src/main/kotlin/it/czerwinski/android/view/layout/tiles/anko/_TilesLayout.kt
+++ tiles-layout-anko/src/main/kotlin/it/czerwinski/android/view/layout/tiles/anko/_TilesLayout.kt
@@ -5,10 +5,37 @@ import android.content.Context
 import android.view.ViewManager
 import it.czerwinski.android.view.layout.tiles.TilesLayout
 import org.jetbrains.anko.custom.ankoView
+import org.jetbrains.anko.internals.AnkoInternals
+
+class _TilesLayout(context: Context) : TilesLayout(context) {
+
+	var padding: Int
+		@Deprecated(AnkoInternals.NO_GETTER, level = DeprecationLevel.ERROR) get() = AnkoInternals.noGetter()
+		set(value) {
+			setPadding(value, value, value, value)
+			innerHorizontalPadding = value
+			innerVerticalPadding = value
+		}
+
+	var horizontalPadding: Int
+		@Deprecated(AnkoInternals.NO_GETTER, level = DeprecationLevel.ERROR) get() = AnkoInternals.noGetter()
+		set(value) {
+			setPadding(value, paddingTop, value, paddingBottom)
+			innerHorizontalPadding = value
+		}
+
+	var verticalPadding: Int
+		@Deprecated(AnkoInternals.NO_GETTER, level = DeprecationLevel.ERROR) get() = AnkoInternals.noGetter()
+		set(value) {
+			setPadding(paddingLeft, value, paddingRight, value)
+			innerVerticalPadding = value
+		}
+
+}
 
 inline fun ViewManager.tilesLayout(cols: Int = 1, rows: Int = 1, theme: Int = 0): TilesLayout = tilesLayout(cols, rows, theme) {}
-inline fun ViewManager.tilesLayout(cols: Int = 1, rows: Int = 1, theme: Int = 0, init: TilesLayout.() -> Unit): TilesLayout {
-	return ankoView({ TilesLayout(it) }, theme) {
+inline fun ViewManager.tilesLayout(cols: Int = 1, rows: Int = 1, theme: Int = 0, init: _TilesLayout.() -> Unit): TilesLayout {
+	return ankoView({ _TilesLayout(it) }, theme) {
 		colsCount = cols
 		rowsCount = rows
 		init()
@@ -16,8 +43,8 @@ inline fun ViewManager.tilesLayout(cols: Int = 1, rows: Int = 1, theme: Int = 0,
 }
 
 inline fun Context.tilesLayout(cols: Int = 1, rows: Int = 1, theme: Int = 0): TilesLayout = tilesLayout(cols, rows, theme) {}
-inline fun Context.tilesLayout(cols: Int = 1, rows: Int = 1, theme: Int = 0, init: TilesLayout.() -> Unit): TilesLayout {
-	return ankoView({ TilesLayout(it) }, theme) {
+inline fun Context.tilesLayout(cols: Int = 1, rows: Int = 1, theme: Int = 0, init: _TilesLayout.() -> Unit): TilesLayout {
+	return ankoView({ _TilesLayout(it) }, theme) {
 		colsCount = cols
 		rowsCount = rows
 		init()
@@ -25,8 +52,8 @@ inline fun Context.tilesLayout(cols: Int = 1, rows: Int = 1, theme: Int = 0, ini
 }
 
 inline fun Activity.tilesLayout(cols: Int = 1, rows: Int = 1, theme: Int = 0): TilesLayout = tilesLayout(cols, rows, theme) {}
-inline fun Activity.tilesLayout(cols: Int = 1, rows: Int = 1, theme: Int = 0, init: TilesLayout.() -> Unit): TilesLayout {
-	return ankoView({ TilesLayout(it) }, theme) {
+inline fun Activity.tilesLayout(cols: Int = 1, rows: Int = 1, theme: Int = 0, init: _TilesLayout.() -> Unit): TilesLayout {
+	return ankoView({ _TilesLayout(it) }, theme) {
 		colsCount = cols
 		rowsCount = rows
 		init()
